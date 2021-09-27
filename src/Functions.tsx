7cac48a8c2ee4d8734481383728e2fd6117d275f
Anthony-jcb <thonyjbc@gmail.com>
2021-09-26 21:26:23 -0400

    Peaky Blinders DB has been created :D

---
+++ src/Functions.tsx
@@ -0,0 +1,33 @@
+const initialDb = [
+  {
+    id: 1,
+    name: "Tommy",
+    role: "Boss",
+  },
+  {
+    id: 2,
+    name: "Arthur",
+    role: "Peaky Blinder",
+  },
+  {
+    id: 3,
+    name: "John",
+    role: "Peaky Blinder",
+  },
+  {
+    id: 4,
+    name: "Michael",
+    role: "Legal Bussines",
+  },
+  {
+    id: 5,
+    name: "Poly",
+    role: "Treasurer",
+  },
+];
+
+function Functions() {
+  return <></>;
+}
+
+export default Functions;
